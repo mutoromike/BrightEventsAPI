dd99ef3c7f8e46348f3dec9f8b6897d11a299610
mutoromike <mikemutoro@gmail.com>
2018-01-09 11:36:49 +0300

    feat(init): add configuration for environments

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
-#app/__init__.py 
+""" app/__init__.py """
 
 from flask import Flask
 from app.useraccounts import UserClass
 from app.events import EventsClass
 from app.eventdetails import EventDetails
+from config import app_config
 
 
-# Initialize the app
+""" Initialize the app """
 app = Flask(__name__, instance_relative_config=True)
-app.secret_key = 'tonystarktheironman'
+# app.secret_key = 'tonystarktheironman'
+app.config.from_object(app_config['development'])
 
 user_object = UserClass()
 events_obj = EventsClass()
@@ -16,5 +18,5 @@ eventdetails_obj = EventDetails()
 
 from app import views
 
-# Load the config file
+""" Load the config file """
 app.config.from_object('config')
