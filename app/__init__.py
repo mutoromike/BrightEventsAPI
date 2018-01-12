b934937fc843ddf9a6530a3fd3d4c98653d339e8
mutoromike <mikemutoro@gmail.com>
2018-01-12 15:00:29 +0300

    feat(app): add configured init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,19 @@
-#app/__init__.py 
+""" app/__init__.py """
 
-from flask import Flask
-from app.useraccounts import UserClass
-from app.events import EventsClass
-from app.eventdetails import EventDetails
+from flask import Flask, request, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from config import app_config
 
+# initialize sql-alchemy
+db = SQLAlchemy()
 
-# Initialize the app
-app = Flask(__name__, instance_relative_config=True)
-app.secret_key = 'tonystarktheironman'
+def create_app(config_name):
+	from app.models import Events
+	# Initialize app
+	app = Flask(__name__, instance_relative_config=True)
 
-user_object = UserClass()
-events_obj = EventsClass()
-eventdetails_obj = EventDetails()
-
-from app import views
-
-# Load the config file
-app.config.from_object('config')
+	#load from config.py in root folder
+	app.config.from_object(app_config[config_name])
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+	db.init_app(app)	
+	return app
