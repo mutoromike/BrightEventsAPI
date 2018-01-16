e44895b33f19dfbf670e9522dda5c0f76270d5c4
mutoromike <mikemutoro@gmail.com>
2018-01-16 10:28:43 +0300

    feat(tests): automate tests

--- manage.py
+++ manage.py
@@ -13,5 +13,18 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+"""
+define command for testing called "test"
+Usage: python manage.py test
+"""
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
 if __name__ == '__main__':
     manager.run()
