62ed1a2b1026a28d546632f6f658563dfd95cc75
mutoromike <mikemutoro@gmail.com>
2018-01-16 10:31:27 +0300

    feat(api): add event creation functionality

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,60 @@ db = SQLAlchemy()
 
 def create_app(config_name):
 	from app.models import Events
-	# Initialize app
+	"""
+	Initialize app
+	"""
 	app = Flask(__name__, instance_relative_config=True)
-
-	#load from config.py in root folder
 	app.config.from_object(app_config[config_name])
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-	db.init_app(app)	
+	db.init_app(app)
+
+	@app.route('/api/v2/events', methods=['POST', 'GET'])
+	def events():
+		if request.method == "POST":
+			event = request.get_json()
+			print(event)
+			created_event = Events(
+									name=event['name'], 
+									category=event['category'], 
+									location=event['location'], 
+									date=event['date'], 
+									description=event['description']
+									)
+			created_event.save()
+			response = jsonify({
+				'id': 'created_event.id',
+				'name' : 'name',
+				'category' : 'category',
+				'location' : 'location',
+				'date' : 'date',
+				'description' : 'description'
+			})
+			response.status_code = 201
+			return response
+		else:
+			""" 
+			GET
+			"""
+			user_id = 1
+			events = Events.get_all(user_id)
+			results = []
+
+			for event in events:
+				obj = {
+					'id': event.id,
+					'name' : name,
+					'category' : category,
+					'location' : location,
+					'date' : date,
+					'description' : description
+				}
+				results.append(obj)
+			response = jsonify(results)
+			response.status_code = 200
+			return response
+
+	from .auth import auth_blueprint
+	app.register_blueprint(auth_blueprint)
+
 	return app
