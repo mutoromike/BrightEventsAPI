4c4be6d5abc29550926b84087be6740db54ccd34
mutoromike <mikemutoro@gmail.com>
2018-02-13 18:24:04 +0300

    feat(db): add blacklist table for tokens

--- app/models.py
+++ app/models.py
@@ -143,5 +143,26 @@ class Events(db.Model):
         return "<Events: {}>".format(self.name)
 
 
+class BlacklistToken(db.Model):
+    """
+    Token Model for storing blacklisted JWT tokens
+    """
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+
+    def __init__(self, token):
+        self.token = token
+
+    def save(self):
+        """Save token"""
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<id: token: {}'.format(self.token)
+
+
 
     
