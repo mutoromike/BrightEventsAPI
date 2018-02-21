b87d99f40df856d26d4128e684b17416d8bb87b3
mutoromike <mikemutoro@gmail.com>
2018-02-21 07:35:37 +0300

    feat(api): modify auth blueprint

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -3,6 +3,6 @@
 from flask import Blueprint
 
 # This instance of a Blueprint that represents the authentication blueprint
-auth_blueprint = Blueprint('auth', __name__)
+auth = Blueprint('auth', __name__)
 
 from . import views
