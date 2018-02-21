69e0597bf5d8e4d2de63c7b2a9e4c797581d692c
mutoromike <mikemutoro@gmail.com>
2018-02-21 07:37:06 +0300

    fix(views): refactor create event method

--- app/events/views.py
+++ app/events/views.py
@@ -23,17 +23,20 @@ def authorize(f):
                     response = {"message": "There was an error creating the event, please try again"}
                     return make_response(jsonify(response)), 500
         response = {"message": "Logged out. Please login again!" }
-        return make_response(jsonify(response)), 200
+        return make_response(jsonify(response)), 401
             
     return check
 
-def checkToken():
-    """Check whether token is blacklisted"""
-    access_token = request.headers.get('Authorization')
-    blacklisted = BlacklistToken.query.filter_by(token=access_token).first()
-    if blacklisted:
-        return True
-    return False
+def validate_data(event):
+    if not event['name'] or not event['category'] or \
+    not event['location'] or not event['date']:
+        return "Event details cannot be empty!"
+
+    elif len(event['name'].strip())<5 or not re.match("^[a-zA-Z0-9_ ]*$", event['name'].strip()):
+        return "Event name cannot have special characters!"    
+
+    else:
+        return event
 
 @events.route('/')
 def index():
@@ -63,40 +66,26 @@ def create(limit=4, page=1):
                 location=event['location']
                 date=event['date']
                 description=event['description']
-
-                if not event['name'] or not event['category'] or \
-                not event['location'] or not event['date']:
-                    # Check whether fields are not empty
-                    response = {"message" : "Event details cannot be empty!"}
-                    return make_response(jsonify(response)), 400
-
-                if len(event['name'].strip())<5 or not re.match("^[a-zA-Z0-9_ ]*$", event['name'].strip()):
-                    response = {"message" : "Event name cannot have special characters!"}
-                    return make_response(jsonify(response)), 400
+                new_event = validate_data(event)
+                if new_event is not event:
+                    return jsonify({"message":new_event}), 400
 
                 existing=Events.query.filter_by(name=name).filter_by(category=category).filter_by\
                 (created_by=user_id).filter_by(location=location).first()
+                
                 if existing:
                     response = {"message" : "A similar event already exists!"}
                     return make_response(jsonify(response)), 302
+                
                 try:
-                    created_event = Events(
-                                    name=name,
-                                    category=category,
-                                    location=location, 
-                                    date=date,
-                                    description=description,
-                                    created_by = user_id
+                    created_event = Events(name=name, category=category, location=location, 
+                                    date=date, description=description, created_by = user_id
                                     )
                     created_event.save()
                     response = jsonify({
-                        'id': created_event.id,
-                        'name' : created_event.name,
-                        'category' : created_event.category,
-                        'location' : created_event.location,
-                        'date' : created_event.date,
-                        'description' : created_event.description,
-                        'created_by' : created_event.created_by
+                        'id': created_event.id, 'name' : created_event.name, 'category' : created_event.category,
+                        'location' : created_event.location, 'date' : created_event.date,
+                        'description' : created_event.description, 'created_by' : created_event.created_by
                     })
 
                 except AttributeError:
@@ -116,13 +105,8 @@ def create(limit=4, page=1):
 
                 for event in events:
                     obj = {
-                        'id': event.id,
-                        'name' : event.name,
-                        'category' : event.category,
-                        'location' : event.location,
-                        'date' : event.date,
-                        'description' : event.description
-
+                        'id': event.id, 'name' : event.name, 'category' : event.category,
+                        'location' : event.location, 'date' : event.date, 'description' : event.description
                     }
                     results.append(obj)
                 return make_response(jsonify(results)), 200
@@ -227,6 +211,7 @@ def event_tasks(event_id):
             return make_response(jsonify(response)), 401
 
 @events.route('/api/v2/event/<event_id>/rsvp', methods=['POST', 'GET'])
+@authorize
 def rsvp(event_id):
     """RSVP to an event"""
     # get the access token from the authorization header
@@ -234,58 +219,44 @@ def rsvp(event_id):
     access_token = auth_header
     if access_token:
         # Get the user id related to this access token
-        user_id = User.decode_token(access_token)
-        token_status = checkToken()
-        if not token_status:
-            # User is still logged in; Continue
-            if not isinstance(user_id, str):
-                # If the id is not a string(error), we have a user id
-                # Get the event with the id specified from the URL (<int:id>)
-                event = Events.query.filter_by(id=event_id).first()
-                if event:
-                    # Check to see if event exists
-                    if request.method == 'POST':
-                        current_user = User.query.filter_by(id=user_id).first()
-                        print(current_user)
-                        result = event.create_reservation(current_user)
-                        print(result)
-                        if result == "Reservation Created":
-                            return jsonify({"message" : "RSVP Successful"}), 201
-                        return jsonify({"message" : "Reservation already created!"}), 302
+        user_id = User.decode_token(access_token)        
+        if not isinstance(user_id, str):
+            # If the id is not a string(error), we have a user id
+            # Get the event with the id specified from the URL (<int:id>)
+            event = Events.query.filter_by(id=event_id).first()
+            if event:
+                # Check to see if event exists
+                if request.method == 'POST':
+                    current_user = User.query.filter_by(id=user_id).first()
+                    print(current_user)
+                    result = event.create_reservation(current_user)
+                    print(result)
+                    if result == "Reservation Created":
+                        return jsonify({"message" : "RSVP Successful"}), 201
+                    return jsonify({"message" : "Reservation already created!"}), 302
 
-                    guests = event.rsvp.all()
-                    created_by = event.created_by
-                    print(created_by)
-                    if user_id == created_by:
-                        if guests:
-                            attending_visitors = []
-                            for user in guests:
-                                new= {
-                                    "username" : user.username,
-                                    "email" : user.email
-                                }
-                                attending_visitors.append(new)
-                            return make_response(jsonify(attending_visitors)), 200
+                guests = event.rsvp.all()
+                created_by = event.created_by
+                print(created_by)
+                if user_id == created_by:
+                    if guests:
+                        attending_visitors = []
+                        for user in guests:
+                            new= { "username" : user.username, "email" : user.email }
+                            attending_visitors.append(new)
+                        return make_response(jsonify(attending_visitors)), 200
 
-                        response = {"message" : "No visitors"}
-                        return make_response(jsonify(response)), 200
-                    else:
-                        response = {
-                            "message": "You can only see visitors of your own event!"
-                        }
-                        return jsonify(response), 401
+                    response = {"message" : "No visitors"}
+                    return make_response(jsonify(response)), 200
                 else:
-                    response = {"message" : "Event does not exist!"}
-                    return make_response(jsonify(response)), 404
+                    response = {"message": "You can only see visitors of your own event!"}
+                    return jsonify(response), 401
             else:
-                response = {"message" : "UNAUTHORIZED! Please login or sign up!"}
-                return make_response(jsonify(response)), 401	
+                response = {"message" : "Event does not exist!"}
+                return make_response(jsonify(response)), 404
         else:
-            response = {"message" : "Logged out. Please login again!"}
-            return make_response(jsonify(response)), 401		
-    else:
-        response = {"message" : "UNAUTHORIZED! Please login or sign up!"}
-        return make_response(jsonify(response)), 401
+            response = {"message" : "UNAUTHORIZED! Please login or sign up!"}
+            return make_response(jsonify(response)), 401	
 
 @events.route('/api/v2/events/all', methods=['GET'])
 @events.route('/api/v2/events/all/page=<int:page>', methods=['GET'])
@@ -298,29 +269,20 @@ def all_events(limit=4, page=1):
     if events:
         for event in events:
             obj = {
-                'id': event.id,
-                'name' : event.name,
-                'category' : event.category,
-                'location' : event.location,
-                'date' : event.date,
-                'description' : event.description
-
+                'id': event.id, 'name' : event.name, 'category' : event.category,
+                'location' : event.location, 'date' : event.date, 'description' : event.description
             }
             results.append(obj)
 
         return make_response(jsonify(results)), 200
     else:
-        results = {
-            "message":"Events not found!"
-        }
+        results = { "message":"Events not found!" }
         return make_response(jsonify(results)), 404
 
 @events.route('/api/v2/search', methods=['POST'])
 @events.route('/api/v2/search/page=<int:page>&limit=<int:limit>', methods=['POST'])
 def search(limit=2, page=1):
     """Search for events in the system"""
-    # events = Events.query.paginate(page, per_page = 6, error_out=True).items
-    # result = request.get_json()
     category = request.args.get("category")
     location = request.args.get("location")
     # get q search value and use if available
